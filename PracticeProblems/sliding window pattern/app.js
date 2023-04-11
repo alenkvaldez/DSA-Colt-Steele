b5805fe7dece0873a3f9a214aeea20af564bd516
Alen Valdez <116237490+alenkvaldez@users.noreply.github.com>
2023-04-11 11:45:00 -0700

    Update app.js

--- PracticeProblems/sliding window pattern/app.js
+++ PracticeProblems/sliding window pattern/app.js
@@ -5,14 +5,20 @@ function maxSubarraySum(arr, num){
     let maxSum = 0;
     let tempSum = 0;
     if (num > arr.length) return null;
-    for (let i =0; i < num; i++){
+    // if invalid array/# too large, return null
+    for (let i = 0; i < num; i++){
+        // start at beginning and sum first digits
         maxSum += arr[i];
+        // store in variable
     }
     tempSum = maxSum;
     for (let i = num; i < arr.length; i++){
         tempSum = tempSum - arr [i - num] + arr[i];
+        // subbing and adding one array value in(array window)
         maxSum = Math.max(maxSum, tempSum);
+        // take maximum(Math.max) bw maxSum and tempSum; updates maxSum if tempSum is larger 
     }
+    // starts loop at next digit
     return maxSum;
 }
 
